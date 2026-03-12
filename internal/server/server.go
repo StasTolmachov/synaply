f230f432b310015b6ddc03faf45b291354f2517c
10493 <stas@tolmachov.dev>
2026-03-12 21:21:29 +0100

    Gracefully handle server shutdown: implement signal handling for SIGINT and SIGTERM, add timeout context for HTTP server shutdown.

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"wordsGo_v2/external/deepl"
@@ -44,7 +48,23 @@ func StartServer(cfg config.Config) {
 		IdleTimeout:  60 * time.Second,
 	}
 
-	if err := httpServer.ListenAndServe(); err != nil {
-		log.Fatalf("Error starting server: %s", err)
+	go func() {
+		slogger.Log.Info("Starting http server on port", "port:", cfg.Api.Port)
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatalf("Error starting server: %s", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
 	}
+	log.Println("Server exiting")
 }
