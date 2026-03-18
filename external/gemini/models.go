f28589addce985507630bcfffc7923ffc41a5ceb
10493 <stas@tolmachov.dev>
2026-03-18 09:00:32 +0100

    Add Gemini table and implement caching logic for word info retrieval

--- external/gemini/models.go
+++ external/gemini/models.go
@@ -1,5 +1,7 @@
 package gemini
 
+import "wordsGo_v2/internal/repository/modelsDB"
+
 type Request struct {
 	SourceLang string `json:"source_lang"`
 	TargetLang string `json:"target_lang"`
@@ -9,4 +11,19 @@ type Request struct {
 
 type Response struct {
 	Response string `json:"response"`
+}
+
+func ReqToGeminiReq(req Request) *modelsDB.GeminiReq {
+	return &modelsDB.GeminiReq{
+		SourceLang: req.SourceLang,
+		TargetLang: req.TargetLang,
+		SourceWord: req.SourceWord,
+		TargetWord: req.TargetWord,
+	}
+}
+
+func GeminiRespToResp(resp *modelsDB.GeminiResp) *Response {
+	return &Response{
+		Response: resp.Response,
+	}
 }
