189f1a94296e0dd56c51adc1d47f43ad3c5167fe
10493 <stas@tolmachov.dev>
2026-03-23 16:18:58 +0100

    Add `languages` import to help page for locale metadata generation

--- frontend_app/src/app/[locale]/help/page.tsx
+++ frontend_app/src/app/[locale]/help/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HelpPageClient from './HelpPageClient';
 import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
+import { languages } from '@/lib/languages';
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }): Promise<Metadata> {
   const { locale } = await params;
