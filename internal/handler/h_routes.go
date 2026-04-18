47ce6f5f7f20d5136e4423d25ddcca2fab06fdb0
Stan <stas@tolmachov.dev>
2026-04-18 13:35:13 +0200

    fix(server, router): update HTTP server timeouts, enhance graceful shutdown, and refactor routes

--- internal/handler/h_routes.go
+++ internal/handler/h_routes.go
@@ -2,12 +2,10 @@ package handler
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-	"github.com/go-chi/httprate"
-	httpSwagger "github.com/swaggo/http-swagger/v2"
 
 	"synaply/internal/middleware"
 )
@@ -15,33 +13,41 @@ import (
 func RegisterRoutes(h *Handler) *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{
-			"https://synaply.me",
-			"http://localhost:3000", // for local frontend development
-		},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
+	r.Use(chiMiddleware.Recoverer)
+	r.Use(chiMiddleware.RealIP)
 
 	r.Use(middleware.RequestLogger())
 	r.Use(middleware.LimitPayloadSize)
 
-	r.Get("/swagger/*", httpSwagger.WrapHandler)
+	// 3. CORS
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{
+			"https://synaply.me",
+			"http://localhost:3000", // для локальной разработки фронтенда
+		},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-Request-ID"},
+		ExposedHeaders:   []string{"Link", "X-Request-ID"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/healthz/live", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status":"ok"}`))
 	})
 
+	r.Get("/healthz/ready", func(w http.ResponseWriter, r *http.Request) {
+		//todo ping to db
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"READY"}`))
+	})
+
 	r.Route("/api/v1/", func(r chi.Router) {
 
 		r.Route("/users", func(r chi.Router) {
-			r.Use(httprate.LimitByIP(5, 1*time.Minute))
 
 		})
 
